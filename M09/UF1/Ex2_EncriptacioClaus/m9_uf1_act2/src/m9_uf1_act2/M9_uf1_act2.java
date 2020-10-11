805c6582c2b7b2af05e2da20a5be9c3eeb05a12a
IanLopezM <72654073+IanLopezM@users.noreply.github.com>
2020-10-11 14:12:37 +0200

    Update M9_uf1_act2.java

--- M09/UF1/Ex2_EncriptacioClaus/m9_uf1_act2/src/m9_uf1_act2/M9_uf1_act2.java
+++ M09/UF1/Ex2_EncriptacioClaus/m9_uf1_act2/src/m9_uf1_act2/M9_uf1_act2.java
@@ -6,43 +6,79 @@
 package m9_uf1_act2;
 
 import java.security.NoSuchAlgorithmException;
+
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import static sun.security.krb5.internal.ktab.KeyTabConstants.keySize;
 
-/**
- *
- * @author Alumne
- */
 public class M9_uf1_act2 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static SecretKey KeygenKeyGeneration(int Keysize){
-    
-        SecretKey sKey = null;
-        if((keySize == 128) || (keySize == 192) || (keySize == 256)){
-            try{
-                KeyGenerator kgen = KeyGenerator.getInstance("AES");
-                kgen.init(keySize);
-                sKey = kgen.generateKey();
-            } catch(NoSuchAlgorithmException ex){
-                System.err.println("Generador no disponible");
+	public static void main(String[] args) throws NoSuchAlgorithmException {
+                System.out.println("\nAlgorisme AES:");
+		byte [] encoded=keygenKeyGeneration(128).getEncoded();
+		for (int i=0; i<encoded.length; i++) {
+			System.out.println(Integer.toBinaryString(encoded[i] & 0xFF).replace(' ', '0')+ "-"+ encoded[i]);
+		}
+                
+		System.out.println("\nAlgorisme DES:");
+		byte [] encoded2=keygenKeyGeneration2(56).getEncoded();
+		
+		for (int j=0; j<encoded2.length; j++) {
+			System.out.println(Integer.toBinaryString(encoded2[j] & 0xFF).replace(' ', '0')+ "-"+ encoded2[j]);
+		}
+                
+                System.out.println("\nAlgorisme DESede:");
+		byte [] encoded3=keygenKeyGeneration3(112).getEncoded();
+		
+		for (int k=0; k<encoded2.length; k++) {
+			System.out.println(Integer.toBinaryString(encoded2[k] & 0xFF).replace(' ', '0')+ "-"+ encoded2[k]);
+		}
+		
+	}
+	
+	public static  SecretKey keygenKeyGeneration(int keySize) {
+            SecretKey sKey = null;
+            if ((keySize == 128 )|| (keySize == 192)|| ( keySize == 256)) {
+                try {
+                    KeyGenerator kgen = KeyGenerator.getInstance("AES");
+                    kgen.init(keySize);
+                    sKey= kgen.generateKey();
+				
+                }catch (NoSuchAlgorithmException ex) {
+                    System.err.println("Generador no disponible.");
+                }
             }
-        }
+            return sKey;
+	} 
+	
+	
+	public static  SecretKey keygenKeyGeneration2(int keySize) {
+            SecretKey sKey = null;
+            if ((keySize == 56 )|| (keySize == 192)|| ( keySize == 256)) {
+                try {
+                    KeyGenerator kgen = KeyGenerator.getInstance("DES");
+                    kgen.init(keySize);
+                    sKey= kgen.generateKey();
+				
+                }catch (NoSuchAlgorithmException ex) {
+                    System.err.println("Generador no disponible" );	
+                }
+            }
+            return sKey;
+	} 
         
-        
-        
-        return sKey;
-    }
-    
-    public static void main(String[] args){
-        
-        int keySz = 128;
-        SecretKey sK = KeygenKeyGeneration(keySz);
-        
-        System.out.println(sK);
-        
-    }
+        public static  SecretKey keygenKeyGeneration3(int keySize) {
+            SecretKey sKey = null;
+            if ((keySize == 112 )|| (keySize == 168)) {
+                try {
+                    KeyGenerator kgen = KeyGenerator.getInstance("DESede");
+                    kgen.init(keySize);
+                    sKey= kgen.generateKey();
+				
+                }catch (NoSuchAlgorithmException ex) {
+                    System.err.println("Generador no disponible" );	
+                }
+            }
+            return sKey;
+	}
+
 }
