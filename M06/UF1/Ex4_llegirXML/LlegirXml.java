3bab6c04081397ee4ae1708e010875bdab764a16
IanLopezM <72654073+IanLopezM@users.noreply.github.com>
2020-10-13 19:42:36 +0200

    Create LlegirXml.java

---
+++ M06/UF1/Ex4_llegirXML/LlegirXml.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package llegirxml;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author Alumne
+ */
+public class LlegirXml {
+
+    /**
+     * @param args the command line arguments
+     * @throws javax.xml.parsers.ParserConfigurationException
+     * @throws org.xml.sax.SAXException
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException{
+        File file = new File("F:\\dam\\m06\\uf1\\ex04\\rows.xml");
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(file);
+
+        Element nodeArrel = doc.getDocumentElement();
+        
+        NodeList list = nodeArrel.getChildNodes();
+        
+        for(int i = 0; i < list.getLength(); i++){
+            //System.out.println(list.);
+        }
+        
+    }
+    
+}
