f438ffe98c88160c33d8acdf50d38eaf3c8d1d55
IanLopezM <72654073+IanLopezM@users.noreply.github.com>
2020-10-20 20:42:16 +0200

    Update M6_uf1_act05.java

--- M06/UF1/Ex5_CrearElements/m6_uf1_act05/src/m6_uf1_act05/M6_uf1_act05.java
+++ M06/UF1/Ex5_CrearElements/m6_uf1_act05/src/m6_uf1_act05/M6_uf1_act05.java
@@ -16,6 +16,14 @@ import java.util.Scanner;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -29,7 +37,7 @@ public class M6_uf1_act5 {
      * @throws org.xml.sax.SAXException
      * @throws java.io.IOException
      */
-    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException{
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException, TransformerConfigurationException, TransformerException{
         // TODO code application logic here
         File file = new File("F:\\dam\\m06\\uf1\\ex05\\alumnes.xml");
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
@@ -37,8 +45,8 @@ public class M6_uf1_act5 {
         Document doc = dBuilder.parse(file);
         
         Scanner sc = new Scanner(System.in);
-        int menu = 0, notaF = 0;
-        String nom, cognom1, cognom2;
+        int menu = 0;
+        String nom, cognom1, cognom2, id, nota;
         
         Element nodeArrel = doc.getDocumentElement();
         
@@ -57,7 +65,34 @@ public class M6_uf1_act5 {
             System.out.println("Segon cognom");
             cognom2 = sc.next();
             System.out.println("Nota Final");
-            notaf = sc.nextInt();
+            nota = sc.next();
+            System.out.println("ID de l alumne");
+            id = sc.next();
+            
+            Element newEl = doc.createElement("alumne");
+            Element nodenom = doc.createElement("nom");
+            Element nodecognom1 = doc.createElement("cognom1");
+            Element nodecognom2 = doc.createElement("cognom2");
+            Element nodenota = doc.createElement("notaFinal");
+            
+            nodenom.appendChild(doc.createTextNode(nom));
+            nodecognom1.appendChild(doc.createTextNode(cognom1));
+            nodecognom2.appendChild(doc.createTextNode(cognom2));
+            nodenota.appendChild(doc.createTextNode(nota));
+            
+            newEl.appendChild(nodenom);
+            newEl.appendChild(nodecognom1);
+            newEl.appendChild(nodecognom2);
+            newEl.appendChild(nodenota);
+            
+            nodeArrel.appendChild(newEl);
+            
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            Result output = new StreamResult(new File("F:\\dam\\m06\\uf1\\ex05\\alumnesnew.xml"));
+            Source input = new DOMSource(doc);
+
+            transformer.transform(input, output);
+            
         }
         
         
