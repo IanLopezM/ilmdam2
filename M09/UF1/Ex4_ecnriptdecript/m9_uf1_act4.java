8703cddc36198dcd064f9cc7875bd1ec89621b3b
IanLopezM <72654073+IanLopezM@users.noreply.github.com>
2020-10-23 15:34:48 +0200

    Update m9_uf1_act4.java

--- M09/UF1/Ex4_ecnriptdecript/m9_uf1_act4.java
+++ M09/UF1/Ex4_ecnriptdecript/m9_uf1_act4.java
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package m9_uf4_act4;
-
+package m9_uf1_act4;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -15,89 +15,108 @@ import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.Scanner;
 import javax.crypto.BadPaddingException;
+
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
-
 /**
  *
- * @author Alumne
+ * @author ianlo
  */
-public class M9_uf4_act4 {
+public class M9_uf1_act4 {
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) throws IOException {
+	//declarem les variables
+        Scanner sc = new Scanner(System.in);
+	String clau, paraulaEncriptada;
+        int kSize = 256; //podría ser 128 o 192 pero he escollit 256
+        
+        //demanem la clau i la paraula/es a encriptar
+	System.out.print("Introdueix la paraula clau: ");
+	clau = sc.nextLine();
+	System.out.print("Introdueix la paraula que vols encriptar i desencriptar: ");
+	paraulaEncriptada = sc.nextLine();
 
-   public static void main(String[] args) {
-	Scanner teclado = new Scanner (System.in);
-	String clau;
-	String contrasenya;
-	System.out.println("Quina es la clau?");
-	clau=teclado.next();
-	System.out.println("Quina es la contrasenya?");
-	contrasenya=teclado.next();
-	//AES
-	//byte [] e1 = passwordKeyGeneration(clau,128).getEncoded();
-	int keySize=256;
-	String texto = clau;
-	SecretKeySpec sKey = null;
-	if ((keySize == 128)|| (keySize==192)||(keySize==256)) {
-		try {
-			
-			byte[] data = texto.getBytes("UTF-8");
-			MessageDigest md = MessageDigest.getInstance("SHA-256");
-			byte [] hash = md.digest(data);
-		    byte [] key = Arrays.copyOf(hash, keySize/8);
-			sKey = new SecretKeySpec(key, "AES");
-			
-		
-			
-		}catch(UnsupportedEncodingException | NoSuchAlgorithmException ex) {
-			System.err.println("Error al generar la clau."+ ex);
-		}
+	//Generem la clau amb la size especificara
+	SecretKey sKey = passwordKeyGeneration(clau, kSize);
+
+	//Agafem la paraula a encriptar, la transformem en bytes 
+        //i cridem a la funció per a encriptar encryptData(sKey, data
+	byte[] data;
+        data = paraulaEncriptada.getBytes();
+	byte[] encryptData;
+        encryptData = encryptData(sKey, data);
+
+        //Ara la desencriptem utilitzant la clau generada 
+        //i el texte encriptat anteriorment
+	byte[] decryptData;
+        decryptData = decryptData(sKey, encryptData);
+
+	//Mostrem per pantalla el texte encriptat i el desencriptat
+	String encripta;
+        encripta = new String(encryptData);
+	System.out.println("La paraula encriptada: " + encripta);
+	String desencripta;
+        desencripta = new String(decryptData);
+	System.out.println("La paraula desencriptada: " + desencripta);
+    }
+
+    // Funcio que donas a la teoria
+    public static SecretKey passwordKeyGeneration(String text, int keySize) {
+	SecretKey sKey = null;
+        
+	if ((keySize == 128) || ( keySize == 192) || (keySize == 256)) {
+            try {
+                byte[] data = text.getBytes("UTF-8");
+                MessageDigest md = MessageDigest.getInstance("SHA-256");
+                byte[] hash = md.digest(data);
+                byte[] key = Arrays.copyOf(hash, keySize/8);
+                sKey = new SecretKeySpec(key, "AES");
+            } catch (UnsupportedEncodingException | NoSuchAlgorithmException ex) {
+                System.out.println("Error generant la clau: " + ex);
+            }
 	}
-	//LLEGIR FITXER 
-	File fitxer = new File ("arxiu.txt");
-	//ECB
-	byte [] e2 = contrasenya.getBytes();
-	byte [] e3 = encryptData( sKey, e2);
-	//MOSTRAR VALOR ENCRIPTAT
-	byte [] e1 = encryptData(sKey, e2);
-	String s1 = new String(e1);
-	System.out.println("missatge encriptat:"+s1);
-	//PASSAR VALORS ENCRIPTATS ECB
-	byte [] e4 = decryptData( sKey,  e1);
-	byte [] e5 = decryptData(sKey, e1);
-	String s = new String(e5);
-	System.out.println("missatge desincriptat:"+s);
+        
+	return sKey;
+    }
+
+    // Metode per a poder encriptar
+    public static byte[] encryptData(SecretKey sKey, byte[] data) {
+	byte[] encryptedData = null;
+        
+	try {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, sKey);
+            encryptedData = cipher.doFinal(data);
+	}catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException ex) {
+            System.err.println("Error xifrant les dades: " + ex);
 	}
-	//ENCRIPTAR
-	public static byte[] encryptData(SecretKey sKey, byte[] data) {
-		byte[] encryptedData = null;
-		
-		try {
-		Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-		cipher.init(Cipher.ENCRYPT_MODE, sKey);
-		encryptedData = cipher.doFinal(data);
-		}
-		catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException ex) {
-		System.err.println("Error xifrant les dades: " + ex);
-		}
-		return encryptedData;
-		}
-	//DESENCRIPTAR
-	public static byte[] decryptData(SecretKey sKey, byte[] data) {
-		byte[] encryptedData = null;
-		
-		try {
-		Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-		cipher.init(Cipher.DECRYPT_MODE, sKey);
-		encryptedData = cipher.doFinal(data);
-		}
-		catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException ex) {
-		System.err.println("Error xifrant les dades: " + ex);
-		}
-		return encryptedData;
-		}
+        
+	return encryptedData;
+    }
+
+
+    // Metode per a poder desencriptar
+    public static byte[] decryptData(SecretKey sKey1, byte[] data) {
+        byte[] encryptedData = null;
+        
+        try {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, sKey1);
+            encryptedData = cipher.doFinal(data);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException ex) {
+            System.err.println("Error desxifrant les dades: " + ex);
+        }
+        
+        return encryptedData;
+    }
 }
