9f039b98a85a38645389d558bc974effce2d2c54
IanLopezM <72654073+IanLopezM@users.noreply.github.com>
2020-10-24 11:54:20 +0200

    Update m9_uf1_act4.java

--- M09/UF1/Ex4_ecnriptdecript/m9_uf1_act4.java
+++ M09/UF1/Ex4_ecnriptdecript/m9_uf1_act4.java
@@ -5,12 +5,16 @@
  */
 package m9_uf1_act4;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -37,14 +41,16 @@ public class M9_uf1_act4 {
     public static void main(String[] args) throws IOException {
 	//declarem les variables
         Scanner sc = new Scanner(System.in);
-	String clau, paraulaEncriptada;
+	String clau, paraulaEncriptada, arxiu;
         int kSize = 256; //podría ser 128 o 192 pero he escollit 256
         
         //demanem la clau i la paraula/es a encriptar
 	System.out.print("Introdueix la paraula clau: ");
 	clau = sc.nextLine();
-	System.out.print("Introdueix la paraula que vols encriptar i desencriptar: ");
-	paraulaEncriptada = sc.nextLine();
+	System.out.print("Introdueix l arxiu que vols encriptar i desencriptar: ");
+	arxiu = sc.nextLine();
+        
+        Path path = Paths.get(arxiu);
 
 	//Generem la clau amb la size especificara
 	SecretKey sKey = passwordKeyGeneration(clau, kSize);
@@ -52,7 +58,7 @@ public class M9_uf1_act4 {
 	//Agafem la paraula a encriptar, la transformem en bytes 
         //i cridem a la funció per a encriptar encryptData(sKey, data
 	byte[] data;
-        data = paraulaEncriptada.getBytes();
+        data = Files.readAllBytes(path);
 	byte[] encryptData;
         encryptData = encryptData(sKey, data);
 
@@ -68,6 +74,34 @@ public class M9_uf1_act4 {
 	String desencripta;
         desencripta = new String(decryptData);
 	System.out.println("La paraula desencriptada: " + desencripta);
+        
+        String[] partesPath = arxiu.split("\\.");
+        String parte0 = partesPath[0];
+        String parte1 = partesPath[1];
+        
+        try{
+            File file = new File(parte0 + "_X." + parte1);
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(encripta);
+            bw.close();
+            System.out.println("La informarcio encriptada s ha guardat a l arxiu " + file);
+        } catch (Exception ex) {
+            System.err.println("Hi ha hagut un error");
+        }
+        
+        try{
+            File file1 = new File(parte0 + "_Y." + parte1);
+            FileWriter fw1 = new FileWriter(file1);
+            BufferedWriter bw1 = new BufferedWriter(fw1);
+            bw1.write(desencripta);
+            bw1.close();
+            System.out.println("La informarcio desencriptada s ha guardat a l arxiu " + file1);
+        } catch (Exception ex) {
+            System.err.println("Hi ha hagut un error");
+        }
+        
+        
     }
 
     // Funcio que donas a la teoria
