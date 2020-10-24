12fedce34f5fafc05909101c0273928ce9054ba2
IanLopezM <72654073+IanLopezM@users.noreply.github.com>
2020-10-24 14:24:17 +0200

    Update M6_uf1_act05.java

--- M06/UF1/Ex5_CrearElements/m6_uf1_act05/src/m6_uf1_act05/M6_uf1_act05.java
+++ M06/UF1/Ex5_CrearElements/m6_uf1_act05/src/m6_uf1_act05/M6_uf1_act05.java
@@ -16,21 +16,26 @@ import java.util.Scanner;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Result;
-import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+
 public class M6_uf1_act5 {
 
+    public static Scanner sc = new Scanner(System.in);
+    public static File file;
+    public static DocumentBuilderFactory dbFactory; 
+    public static DocumentBuilder dBuilder;
+    public static Document doc;
+    public static Element nodeArrel;
+    
     /**
      * @param args the command line arguments
      * @throws javax.xml.parsers.ParserConfigurationException
@@ -38,83 +43,71 @@ public class M6_uf1_act5 {
      * @throws java.io.IOException
      * @throws javax.xml.transform.TransformerConfigurationException
      */
-    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException, TransformerConfigurationException, TransformerException{
-        // TODO code application logic here
-        File file = new File("F:\\dam\\m06\\uf1\\ex05\\alumnes.xml");
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-        Document doc = dBuilder.parse(file);
-        
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+    
+        boolean fin = false;
         int menu = 0;
-        String nom, cognom1, cognom2, id, nota;
         
-        Element nodeArrel = doc.getDocumentElement();
+        file = new File("E:\\dam\\m06\\uf1\\ex05\\alumnes.xml");
+	dbFactory = DocumentBuilderFactory.newInstance();
+	dBuilder = dbFactory.newDocumentBuilder();
+	doc = dBuilder.parse(file);
+	nodeArrel = doc.getDocumentElement();
         
-        NodeList list = nodeArrel.getChildNodes();
-        
-        System.out.println("1- crear element");
-        System.out.println("2- modificar element");
-        System.out.println("3- eliminar element");
-        menu = sc.nextInt();
-        
-        if(menu == 1){
-            System.out.println("Nom de l alumne");
-            nom = sc.next();
-            System.out.println("Primer cognom");
-            cognom1 = sc.next();
-            System.out.println("Segon cognom");
-            cognom2 = sc.next();
-            System.out.println("Nota Final");
-            nota = sc.next();
-            System.out.println("ID de l alumne");
-            id = sc.next();
+        while(!fin){
+            System.out.println("1- Afegir un node o atribut\n"
+                    + "2- Modificar un node o atribut\n"
+                    + "3- Eliminar un node o atribut"
+                    + "4- Mostrar l estructura de l XML"
+                    + "5- Finalitzar el programa");
+            menu = sc.nextInt();
             
-            Element newEl = doc.createElement("alumne");
-            Element nodenom = doc.createElement("nom");
-            Element nodecognom1 = doc.createElement("cognom1");
-            Element nodecognom2 = doc.createElement("cognom2");
-            Element nodenota = doc.createElement("notaFinal");
-            
-            newEl.setAttribute("id", id);
-            newEl.setIdAttribute("id", true);
-            
-            nodenom.appendChild(doc.createTextNode(nom));
-            nodecognom1.appendChild(doc.createTextNode(cognom1));
-            nodecognom2.appendChild(doc.createTextNode(cognom2));
-            nodenota.appendChild(doc.createTextNode(nota));
-            
-            
-            newEl.appendChild(nodenom);
-            newEl.appendChild(nodecognom1);
-            newEl.appendChild(nodecognom2);
-            newEl.appendChild(nodenota);
-            
-            nodeArrel.appendChild(newEl);
-        } else if (menu == 2) {
-            String idAlumno;
-            System.out.println("Quin es l alumne que vols modificar?");
-            idAlumno = sc.next();
-            Element elementoAhora = doc.getElementById(idAlumno);
-            System.out.println("Quin node vols modificar:");
-            String nodo = sc.next();
-            System.out.println("Nou nom per al node");
-            String nouNom = sc.next();
-            
-            NodeList nodosAhora = elementoAhora.getChildNodes();
-            
-            for (int i = 0; i < nodosAhora.getLength(); i++) {
-		if (nodosAhora.item(i).getNodeName().equals(nodo)) {
-                    nodosAhora.item(i).setTextContent(nouNom);
+            if(menu == 1){
+                boolean fin1 = false;
+                int opcio = 0;
+                while(!fin1){
+                    System.out.println("Vols afegir un element(1) o un atribut(2)");
+                    opcio = sc.nextInt();
+                    
+                    if(opcio == 1){
+                        String id, nom, cognom1, cognom2, nota;
+			System.out.println("Quin sera l id de l alumne");
+			id = sc.next();			
+			System.out.println("Quin es el seu nom");
+			nom = sc.next();	
+			System.out.println("Quin es el seu primer cognom");
+			cognom1 = sc.next();	
+			System.out.println("Quin es el seu segon cognom");
+			cognom2 = sc.next();		
+			System.out.println("Quina es la seva nota final");
+			nota = sc.next();
+                        
+                        Element elementNom = doc.createElement("nom");
+                        Element elementCognom1 = doc.createElement("cognom1");
+                        Element elementCognom2 = doc.createElement("cognom2");
+                        Element elementNota = doc.createElement("nota");
+                        Element element = doc.createElement("alumne");
+                        
+			elementNom.appendChild(doc.createTextNode(nom));	
+			elementCognom1.appendChild(doc.createTextNode(cognom1));
+			elementCognom2.appendChild(doc.createTextNode(cognom2));
+			elementNota.appendChild(doc.createTextNode(nota));
+                        nodeArrel.appendChild(element);
+                        
+                        element.setAttribute("id", id);
+			element.setIdAttribute("id", true);
+			element.appendChild(elementNom);
+			element.appendChild(elementCognom1);
+			element.appendChild(elementCognom2);
+			element.appendChild(elementNota);
+                        
+                        fin1 = true;
+                    }
                 }
             }
+            
         }
         
-            Transformer transformer = TransformerFactory.newInstance().newTransformer();
-            Result output = new StreamResult(new File("F:\\dam\\m06\\uf1\\ex05\\alumnesnew.xml"));
-            Source input = new DOMSource(doc);
-
-            transformer.transform(input, output);
     }
     
 }
