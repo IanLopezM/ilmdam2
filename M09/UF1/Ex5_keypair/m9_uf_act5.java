0a9f674eba64263585c9c503c4024a25a20e8b59
IanLopezM <72654073+IanLopezM@users.noreply.github.com>
2020-10-30 19:52:16 +0100

    Create m9_uf_act5.java

---
+++ M09/UF1/Ex5_keypair/m9_uf_act5.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m9_uf1_act5;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import javax.crypto.Cipher;
+
+/**
+ *
+ * @author Alumne
+ */
+public class M9_uf1_act5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public KeyPair randomGenerate(int longuitudClau) {
+        KeyPair keys = null;
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+            keyGen.initialize(longuitudClau);
+            keys = keyGen.genKeyPair();
+        }
+        catch (Exception ex) {
+            System.err.println("Generador no disponible.");
+        }
+        return keys;
+    }
+
+    public byte[] encryptData(byte[] data, PublicKey pub) {
+        byte[] encryptedData = null;
+        try {
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding","SunJCE");
+            cipher.init(Cipher.ENCRYPT_MODE, pub);
+            encryptedData = cipher.doFinal(data);
+        }
+        catch (Exception ex) {
+            System.err.println("Error xifrant: " + ex);
+        }
+        return encryptedData;
+    }
+    
+    public byte[] decryptData(byte[] data, PrivateKey pub) {
+        byte[] decryptData = null;
+        try {
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding","SunJCE");
+            cipher.init(Cipher.DECRYPT_MODE, pub);
+            decryptData = cipher.doFinal(data);
+        }
+        catch (Exception ex) {
+            System.err.println("Error xifrant: " + ex);
+        }
+        return decryptData;
+    }
+    
+}
