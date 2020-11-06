41bd43f83bcfb828dc335f93dd7f092deaaf47ec
IanLopezM <72654073+IanLopezM@users.noreply.github.com>
2020-11-06 20:40:42 +0100

    Update Generacio.java

--- M09/UF1/Ex6_clauEmbolcallada/m9_uf1_act6/src/m9_uf1_act6/Generacio.java
+++ M09/UF1/Ex6_clauEmbolcallada/m9_uf1_act6/src/m9_uf1_act6/Generacio.java
@@ -5,10 +5,40 @@
  */
 package m9_uf1_act6;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
 /**
  *
  * @author ianlo
  */
 public class Generacio {
     
+    public static void main(String[] args) {
+        int kSize = 512;
+        KeyPair parellDeClaus;
+        PrivateKey clauPrivada;
+        PublicKey clauPublica;
+        
+        parellDeClaus = randomGenerate(kSize);
+        clauPrivada = parellDeClaus.getPrivate();
+        clauPublica = parellDeClaus.getPublic();
+        
+    }
+    
+    public static KeyPair randomGenerate(int longuitudClau) {
+        KeyPair keys = null;
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+            keyGen.initialize(longuitudClau);
+            keys = keyGen.genKeyPair();
+        }
+        catch (Exception ex) {
+            System.err.println("Generador no disponible.");
+        }
+        return keys;
+    }
 }
+
