a7519bde2bf49d16fe310e3be8d8382085c0c620
IanLopezM <72654073+IanLopezM@users.noreply.github.com>
2020-11-07 15:06:38 +0100

    Update Encriptacio.java

--- M09/UF1/Ex6_clauEmbolcallada/m9_uf1_act6/src/m9_uf1_act6/Encriptacio.java
+++ M09/UF1/Ex6_clauEmbolcallada/m9_uf1_act6/src/m9_uf1_act6/Encriptacio.java
@@ -7,9 +7,14 @@ package m9_uf1_act6;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 
 /**
@@ -22,21 +27,24 @@ public class Encriptacio {
         Scanner sc = new Scanner(System.in);
         String arxiuPublica, textePerPantalla;
         Path pathArxiuPublica;
-        byte[] clauSimetrica;
+        SecretKey clauSimetrica;
+        byte[] dataTextePerPantalla;
         
         System.out.println("Digues el nom de l arxiu de la clau publica");
         arxiuPublica = sc.nextLine();
-        
         pathArxiuPublica = Paths.get(arxiuPublica);
         
-        clauSimetrica = keyGenerator().getEncoded();
+        clauSimetrica = keyGenerator();
        
+        System.out.println("Quin es el texte que vols encriptar?");
+        textePerPantalla = sc.nextLine();
+        dataTextePerPantalla = textePerPantalla.getBytes();
+        
     }
     
     public static SecretKey keyGenerator() throws NoSuchAlgorithmException{
         SecretKey sKey = null;
         int kSize = 256;
-        
         try{
             KeyGenerator kgen = KeyGenerator.getInstance("AES");
             kgen.init(kSize);
@@ -45,6 +53,24 @@ public class Encriptacio {
             System.err.println("Generador no disponible.");
         }
         return sKey;
+    }
+    
+    public static byte[] encryptData(SecretKey sKey, byte[] data) 
+            throws InvalidKeyException, NoSuchPaddingException, 
+            IllegalBlockSizeException, BadPaddingException {
+	byte[] encryptedData = null;
+        
+	try {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, sKey);
+            encryptedData = cipher.doFinal(data);
+	}catch (NoSuchAlgorithmException | NoSuchPaddingException | 
+                InvalidKeyException | IllegalBlockSizeException |
+                BadPaddingException ex) {
+            System.err.println("Error xifrant les dades: " + ex);
+	}
+        
+	return encryptedData;
     }
     
 }
