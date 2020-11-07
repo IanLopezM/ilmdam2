2f760001bb8c0090924e3e92887b153895e6c895
IanLopezM <72654073+IanLopezM@users.noreply.github.com>
2020-11-07 03:24:14 +0100

    Update Encriptacio.java

--- M09/UF1/Ex6_clauEmbolcallada/m9_uf1_act6/src/m9_uf1_act6/Encriptacio.java
+++ M09/UF1/Ex6_clauEmbolcallada/m9_uf1_act6/src/m9_uf1_act6/Encriptacio.java
@@ -7,7 +7,10 @@ package m9_uf1_act6;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 
 /**
  *
@@ -15,15 +18,33 @@ import java.util.Scanner;
  */
 public class Encriptacio {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
         Scanner sc = new Scanner(System.in);
-        String arxiuPublica;
+        String arxiuPublica, textePerPantalla;
         Path pathArxiuPublica;
-    
+        byte[] clauSimetrica;
+        
         System.out.println("Digues el nom de l arxiu de la clau publica");
         arxiuPublica = sc.nextLine();
         
         pathArxiuPublica = Paths.get(arxiuPublica);
+        
+        clauSimetrica = keyGenerator().getEncoded();
        
     }
+    
+    public static SecretKey keyGenerator() throws NoSuchAlgorithmException{
+        SecretKey sKey = null;
+        int kSize = 256;
+        
+        try{
+            KeyGenerator kgen = KeyGenerator.getInstance("AES");
+            kgen.init(kSize);
+            sKey = kgen.generateKey();
+        } catch (Exception ex){
+            System.err.println("Generador no disponible.");
+        }
+        return sKey;
+    }
+    
 }
