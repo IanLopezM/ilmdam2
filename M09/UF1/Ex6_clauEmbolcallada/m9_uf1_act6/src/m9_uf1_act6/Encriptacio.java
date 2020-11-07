275a33c5a61b20a3832fe577b2f564d559290ba0
IanLopezM <72654073+IanLopezM@users.noreply.github.com>
2020-11-07 02:48:10 +0100

    Update Encriptacio.java

--- M09/UF1/Ex6_clauEmbolcallada/m9_uf1_act6/src/m9_uf1_act6/Encriptacio.java
+++ M09/UF1/Ex6_clauEmbolcallada/m9_uf1_act6/src/m9_uf1_act6/Encriptacio.java
@@ -1,1 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m9_uf1_act6;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ianlo
+ */
+public class Encriptacio {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String arxiuPublica;
+        Path pathArxiuPublica;
+    
+        System.out.println("Digues el nom de l arxiu de la clau publica");
+        arxiuPublica = sc.nextLine();
+        
+        pathArxiuPublica = Paths.get(arxiuPublica);
+       
+    }
+}
