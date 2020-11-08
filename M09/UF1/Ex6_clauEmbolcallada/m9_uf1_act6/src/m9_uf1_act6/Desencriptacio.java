ae996a6bd8089ea98c3015334b10f7f5ab2d92f5
IanLopezM <72654073+IanLopezM@users.noreply.github.com>
2020-11-08 18:07:32 +0100

    Update Desencriptacio.java

--- M09/UF1/Ex6_clauEmbolcallada/m9_uf1_act6/src/m9_uf1_act6/Desencriptacio.java
+++ M09/UF1/Ex6_clauEmbolcallada/m9_uf1_act6/src/m9_uf1_act6/Desencriptacio.java
@@ -7,7 +7,6 @@ package m9_uf1_act6;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
@@ -15,6 +14,9 @@ import java.security.PrivateKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 /**
  *
@@ -24,35 +26,51 @@ public class Desencriptacio {
     
     public static void main(String[] args) throws NoSuchAlgorithmException, 
             InvalidKeySpecException, IOException{
-        
-        byte[] dataPrivada;
+        KeyFactory keyFactory;
+        PKCS8EncodedKeySpec privateKeySpec;
+        String privateKeyContent;
+        PrivateKey privateKey;
+        SecretKey secretKey;
+        byte[] dataPrivada, dataArxiuEncriptat, dataSimetricaEncriptada,
+                privateKeyDecoded, dataDecryptKey;
         dataPrivada = Files.readAllBytes(Paths.get("clauPRIVADA"));
 
-        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-
-        // Clean private key information
-        String privateKeyContent = new String(dataPrivada);
+        keyFactory= KeyFactory.getInstance("RSA");
+        privateKeyContent = new String(dataPrivada);
         privateKeyContent = privateKeyContent
                 .replace("—-BEGIN RSA PRIVATE KEY—-", "")
                 .replace("—-END RSA PRIVATE KEY—-", "")
                 .replace("\n", "");
-
-        // Get all bytes decoded from the private key content
-        byte[] privateKeyDecoded = Base64.getDecoder()
+        privateKeyDecoded = Base64.getDecoder()
                 .decode(privateKeyContent);
-
-        // Generate private key object from bytes
-        PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateKeyDecoded);
-        PrivateKey privateKey = keyFactory.generatePrivate(privateKeySpec);
+        privateKeySpec = new PKCS8EncodedKeySpec(privateKeyDecoded);
+        privateKey = keyFactory.generatePrivate(privateKeySpec);
         
+        dataArxiuEncriptat = Files
+                .readAllBytes(Paths.get("ZZZ_missatge_encriptat"));
         
+        dataSimetricaEncriptada = Files
+                .readAllBytes(Paths.get("ZZZ_clau_encriptada"));
         
-        
+        dataDecryptKey = decryptData(dataSimetricaEncriptada, privateKey);
+        secretKey = new SecretKeySpec(dataDecryptKey, 0, 
+                 dataDecryptKey.length, "AES");
         
         
     }
     
-    
+    public static byte[] decryptData(byte[] data, PrivateKey priv) {
+        byte[] encryptedData = null;
+        try {
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding","SunJCE");
+            cipher.init(Cipher.DECRYPT_MODE, priv);
+            encryptedData = cipher.doFinal(data);
+        }
+        catch (Exception ex) {
+            System.err.println("Error xifrant: " + ex);
+        }
+        return encryptedData;
+    }
     
     
 }
