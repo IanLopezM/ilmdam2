356b167d6b9ed9610ba0f5e332606f72200b7604
IanLopezM <72654073+IanLopezM@users.noreply.github.com>
2020-11-22 15:51:42 +0100

    Update M9_uf1_act8.java
    cargo el almacen de llaves

--- M09/UF1/Ex8_CertificatDigital/M9_uf1_act8.java
+++ M09/UF1/Ex8_CertificatDigital/M9_uf1_act8.java
@@ -7,8 +7,11 @@ package m9_uf1_act8;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 import java.util.Scanner;
 
 
@@ -20,16 +23,24 @@ public class M9_uf1_act8 {
 
     /**
      * @param args the command line arguments
+     * @throws java.security.KeyStoreException
+     * @throws java.io.FileNotFoundException
+     * @throws java.security.NoSuchAlgorithmException
+     * @throws java.security.cert.CertificateException
      */
     public static void main(String[] args) throws KeyStoreException, 
-            FileNotFoundException{
+            FileNotFoundException, IOException, NoSuchAlgorithmException,
+            CertificateException{
         // TODO code application logic here
         
+        //declaració de variables
         KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
         Scanner sc = new Scanner(System.in);
         FileInputStream fis = new FileInputStream("E:/dam/m09/uf1/act8/llavesianlopez");
         String contrasenya = "123456";
         char[] arrayContrasenya = contrasenya.toCharArray();
+    
+        ks.load(fis, arrayContrasenya);
     }
     
 }
