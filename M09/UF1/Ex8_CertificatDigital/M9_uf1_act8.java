e254078348e629b7b36bf0de437393ca2b7405e8
IanLopezM <72654073+IanLopezM@users.noreply.github.com>
2020-11-22 15:49:31 +0100

    Update M9_uf1_act8.java

--- M09/UF1/Ex8_CertificatDigital/M9_uf1_act8.java
+++ M09/UF1/Ex8_CertificatDigital/M9_uf1_act8.java
@@ -5,8 +5,11 @@
  */
 package m9_uf1_act8;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
+import java.util.Scanner;
 
 
 /**
@@ -18,11 +21,15 @@ public class M9_uf1_act8 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws KeyStoreException{
+    public static void main(String[] args) throws KeyStoreException, 
+            FileNotFoundException{
         // TODO code application logic here
         
         KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+        Scanner sc = new Scanner(System.in);
+        FileInputStream fis = new FileInputStream("E:/dam/m09/uf1/act8/llavesianlopez");
+        String contrasenya = "123456";
+        char[] arrayContrasenya = contrasenya.toCharArray();
     }
     
 }
-
