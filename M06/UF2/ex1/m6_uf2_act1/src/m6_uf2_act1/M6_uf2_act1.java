420f09b3afa38c780ca39aa5cc53db9baf07cfca
IanLopezM <72654073+IanLopezM@users.noreply.github.com>
2020-11-24 20:18:06 +0100

    Add files via upload
    He creat la base de dades amb la taula alumnes des de phpmyadmin.

---
+++ M06/UF2/ex1/m6_uf2_act1/src/m6_uf2_act1/M6_uf2_act1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m6_uf2_act1;
+
+/**
+ *
+ * @author ianlo
+ */
+public class M6_uf2_act1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
