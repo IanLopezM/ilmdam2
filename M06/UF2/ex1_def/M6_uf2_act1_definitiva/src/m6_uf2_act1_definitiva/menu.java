01beb6bacc697800b8548b06da768e0acf9d3a71
Alumne <Alumne@Ai104-PC08>
2020-12-01 20:44:16 +0100

    despues de apagón en clase

--- M06/UF2/ex1_def/M6_uf2_act1_definitiva/src/m6_uf2_act1_definitiva/menu.java
+++ M06/UF2/ex1_def/M6_uf2_act1_definitiva/src/m6_uf2_act1_definitiva/menu.java
@@ -53,6 +53,7 @@ public class menu extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(204, 204, 255));
+        jPanel1.setMinimumSize(new java.awt.Dimension(800, 500));
 
         jPanel2.setBackground(new java.awt.Color(255, 255, 204));
         jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
