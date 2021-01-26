b142648133b5c02031b18c99bbca88cfed438432
Alumne <Alumne@Ai104-PC08>
2021-01-26 19:47:35 +0100

    commit apartado 1 acabado

--- M09/UF2/m9_uf2_act4/m9_uf2_act4/src/m9_uf2_act4/M9_uf2_act4.java
+++ M09/UF2/m9_uf2_act4/m9_uf2_act4/src/m9_uf2_act4/M9_uf2_act4.java
@@ -8,8 +8,11 @@ package m9_uf2_act4;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  *
@@ -17,17 +20,39 @@ import java.util.concurrent.ThreadPoolExecutor;
  */
 public class M9_uf2_act4 {
     
-    static class Client implements Callable<Integer> {
+    static class Client implements Runnable {
         
-        private int totalProductos;
+        private int numeroCliente;
+        private int[] timposPosibles = {2, 3, 4, 5, 6, 7, 8};
+        private int articlesRandom = (int) (Math.random() * (30 - 1 + 1)) + 1;
         
-        public Client(int productos) {
-            totalProductos = productos;
+        public Client(int numeroCliente) {
+            this.numeroCliente = numeroCliente;
+            System.out.println("Creat el client " + numeroCliente + " amb " + articlesRandom + " articles");            
         }
-        
+
         @Override
-        public Integer call() throws Exception {
-            return 0;
+        public void run() {
+            int unsegundo = 1000;
+            int tempsRandom;
+            
+            System.out.println("Client " + numeroCliente + " passa per caixa ...");
+            for (int j = 1; j < articlesRandom + 1; j++) {
+                tempsRandom = (int) (Math.random() * (6 - 0 + 1));
+                try {
+                    Thread.sleep(timposPosibles[tempsRandom] * unsegundo);
+                }catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                
+                System.out.print("Client " + numeroCliente + " article " + j + "/" + articlesRandom + " (" + timposPosibles[tempsRandom] + " segons)");
+                
+                if (j == articlesRandom) {
+                    System.out.print(" FINALITZAT");
+                }
+                
+                System.out.println("");
+            }
         }
     }
     
@@ -35,17 +60,21 @@ public class M9_uf2_act4 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         // TODO code application logic here
-        int TOTAL_CLIENTS = 50;
-        int nombreArticles, tempsArticle;
-        Client[] arrayClient = new Client[TOTAL_CLIENTS];
+        int totalClientes = 50;
+        int miliSegundos = 3000;
+        ScheduledThreadPoolExecutor executor = (ScheduledThreadPoolExecutor) Executors.newScheduledThreadPool (5);
         
-        ThreadPoolExecutor executor = (ThreadPoolExecutor)  
-                Executors.newFixedThreadPool(50);
-        List<Client> llistaTasques = new ArrayList<Client>();
-        
-        
+        for(int i = 0; i <= totalClientes; i++) {
+            Client task = new Client(i);
+            Thread.sleep(miliSegundos);
+            executor.execute(task);
+            //executor.scheduleWithFixedDelay(task, 0, 3, TimeUnit.SECONDS);
+            //executor.schedule(task, 3, TimeUnit.SECONDS);
+        }
+       
+        executor.shutdown();
         
     }
     
