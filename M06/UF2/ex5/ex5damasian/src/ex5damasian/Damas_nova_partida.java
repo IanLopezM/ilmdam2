2f8ddc8f28203ad214b0e47408ac9751a5fea897
Alumne <Alumne@Ai104-PC08>
2021-01-28 19:45:49 +0100

    empezando hibernate (3.3)

--- M06/UF2/ex5/ex5damasian/src/ex5damasian/Damas_nova_partida.java
+++ M06/UF2/ex5/ex5damasian/src/ex5damasian/Damas_nova_partida.java
@@ -9,6 +9,10 @@ import entity.Movimiento;
 import entity.Partida;
 import javax.swing.JOptionPane;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
 
 /**
  *
@@ -23,6 +27,14 @@ public class Damas_nova_partida extends javax.swing.JFrame {
     int filaDesti = -1;
     int columnaDesti = -1;
     Partida partida;
+    
+    SessionFactory sessionFactory;
+ 
+  Configuration configuration = new Configuration();
+  configuration.configure("hibernate.cfg.xml");
+  ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+  sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+    
     
     /**
      * Creates new form Damas_nova_partida
