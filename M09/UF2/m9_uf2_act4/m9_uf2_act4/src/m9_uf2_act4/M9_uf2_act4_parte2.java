b7cd2a359dfb5f02a5eb459c99c7d9893fb0996b
= <=>
2021-01-31 16:33:59 +0100

    vamos bien

--- M09/UF2/m9_uf2_act4/m9_uf2_act4/src/m9_uf2_act4/M9_uf2_act4_parte2.java
+++ M09/UF2/m9_uf2_act4/m9_uf2_act4/src/m9_uf2_act4/M9_uf2_act4_parte2.java
@@ -5,33 +5,46 @@
  */
 package m9_uf2_act4;
 
+import java.util.Arrays;
+
 /**
  *
  * @author Alumne
  */
 public class M9_uf2_act4_parte2 {
     
-    public int[] array = new int[100];
-    
-    
-    public static int[] bombolla(int[] arreglo)
-    {
-      int auxiliar;
-      int[] arregloOrdenado;
-      for(int i = 2; i < arreglo.length; i++)
-      {
-        for(int j = 0;j < arreglo.length-i;j++)
-        {
-          if(arreglo[j] > arreglo[j+1])
-          {
-            auxiliar = arreglo[j];
-            arreglo[j] = arreglo[j+1];
-            arreglo[j+1] = auxiliar;
-          }   
+    static class GrupBombolla implements Runnable {
+        int grup;
+        int[] grupBombolla;
+        
+        public GrupBombolla(int grup, int[]grupBombolla) {
+            this.grup = grup;
+            this.grupBombolla = grupBombolla;
         }
-      }
-      arregloOrdenado = arreglo;
-      return arregloOrdenado;
+        
+        @Override
+        public void run() {
+            int grupAux, i, j;
+            int[] grupBombollaAux;
+            
+            for(i = 1; i < grupBombolla.length; i++){
+                for(j = 0; j < grupBombolla.length; j++){
+                    if(grupBombolla[j+1] < grupBombolla[j]){
+                        grupBombollaAux = grupBombolla.clone();
+                        grupAux = grupBombolla[j];
+                        grupBombolla[j] = grupBombolla[j+1];
+                        grupBombolla[j+1] = grupAux;
+                        System.out.println("El grup " + grup + " " + 
+                                Arrays.toString(grupBombollaAux) + 
+                                " pasa a ser " +
+                                Arrays.toString(grupBombolla));
+                    }
+                }
+            }
+            System.out.println("L'array resultant es " + 
+                    Arrays.toString(grupBombolla));
+        }
     }
+    
     
 }
