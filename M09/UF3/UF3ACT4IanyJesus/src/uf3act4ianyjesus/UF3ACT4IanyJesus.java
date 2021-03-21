9ce09cbbf5291f485d1ba4786ac96291a2e825a0
= <=>
2021-03-21 18:53:08 +0100

    empezamos el 4, el servidor será example2URL

--- M09/UF3/UF3ACT4IanyJesus/src/uf3act4ianyjesus/UF3ACT4IanyJesus.java
+++ M09/UF3/UF3ACT4IanyJesus/src/uf3act4ianyjesus/UF3ACT4IanyJesus.java
@@ -5,6 +5,15 @@
  */
 package uf3act4ianyjesus;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 /**
  *
  * @author ianlo
@@ -13,9 +22,39 @@ public class UF3ACT4IanyJesus {
 
     /**
      * @param args the command line arguments
+     * @throws java.net.SocketException
+     * @throws java.net.UnknownHostException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SocketException, UnknownHostException, IOException {
         // TODO code application logic here
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        //Socket client
+        DatagramSocket clientSocket = new DatagramSocket();
+        byte[] enviats = new byte[1024];
+        byte[] rebuts = new byte[1024];
+        String cadena;
+        boolean mescincsec = true;
+
+        //DADES DEL SERVIDOR al qual s'envia el missatge
+        InetAddress IPServidor = InetAddress.getLocalHost();
+        int port = 9800;
+
+        while (mescincsec) {
+            //INTRODUIR DADES PEL TECLAT
+            System.out.print("Introdueix missatge: ");
+            cadena = in.readLine();
+            enviats = cadena.getBytes();
+
+            //ENVIANT DATAGRAMA AL SERVIDOR
+            System.out.println("Enviant " + enviats.length
+                    + "bytes al servidor.");
+            DatagramPacket enviament
+                    = new DatagramPacket(enviats, enviats.length, IPServidor,
+                            port);
+            clientSocket.send(enviament);
+
+        }
+
     }
-    
+
 }
