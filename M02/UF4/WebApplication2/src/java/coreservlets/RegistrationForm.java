adae4ab85981613300c5a99a8fd31930c4cb2258
Alumne <Alumne@Ai104-PC08>
2021-04-16 19:49:47 +0200

    pt2 finish creation of files

--- M02/UF4/WebApplication2/src/java/coreservlets/RegistrationForm.java
+++ M02/UF4/WebApplication2/src/java/coreservlets/RegistrationForm.java
@@ -8,6 +8,7 @@ package coreservlets;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,6 +17,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Alumne
  */
+@WebServlet("/registro-form")
 public class RegistrationForm extends HttpServlet {
 
     /**
